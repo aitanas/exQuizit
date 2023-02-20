bab9c7ec36d18c71e0c04e1ca05b19cb41288b9b
Aitana Shough <aeshough@gmail.com>
2023-02-20 13:25:15 -0800

    update gitignore and add basic component scaffolding
    Co-authored-by: Brian Noh <noh24@ymail.com>
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>
    Co-authored-by: Richard Cha <charichard09@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Footer from './Footer';
+import HomeControl from './HomeControl';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomeControl />
+      <Footer />
+    </div>
+  );
+}
