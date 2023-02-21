272afc8390c14d45e5f5c376671a3ce5bc46b80b
Aitana Shough <aeshough@gmail.com>
2023-02-21 09:20:46 -0800

    add functionality to display data from db to collection page
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>
    Co-authored-by: Richard Cha <charichard09@gmail.com>
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- src/components/flashcard/Collection.js
+++ src/components/flashcard/Collection.js
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from "./Card";
 import { collection, query, addDoc, onSnapshot, doc, updateDoc, getDocs } from "firebase/firestore";
 import db from '../../firebase';
 
-export default function Collection() { // 
+export default  function Collection() { // 
 //   const collections = [];
 
 //   getData() async {
 //     await 
 //   }
+  const [cards, setCards] = useState([]);
+
   useEffect(() => {
+    let array = [];
     async function fetch() {
       const collectionArray = await getDocs(collection(db, 'flashcards'));
       collectionArray.forEach((doc) => {
-        console.log(doc.data())
+        array.push({...doc.data(), id: doc.id})
+        // console.log(array);
+        setCards(array);
       })
     }
     fetch();
   },[])
-
   // const collections = query(collection(db, "flashcards"));
   // const collectionsSnapshot = await getDocs(collections);
 
@@ -30,6 +34,12 @@ export default function Collection() { //
   return (
     <div>
       <ul>
+        {cards.map((card) => (
+          <div key={card.id}>
+            <div>{card.question}</div>
+            <div>{card.answer}</div>
+          </div>
+        ))}
       </ul>       
     </div>
   )
